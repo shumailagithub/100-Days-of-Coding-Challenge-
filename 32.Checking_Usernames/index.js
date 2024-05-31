e69602914731e3c363c98489fb4c4f5ecdbe202c
ComTech <shumailagulfam3@gmail.com>
2024-05-31 18:40:13 +0500

    first commit

--- 32.Checking_Usernames/index.js
+++ 32.Checking_Usernames/index.js
@@ -1,54 +1,27 @@
 //Q No 32.
 //Checking Usernames: Do the following to create a program that simulates how websites ensure that everyone has a unique username.
 //• Make a list of five or more usernames called current_users.
-
 //• Make another list of five usernames called new_users. Make sure one or two of the new usernames are also in the current_users list.
-
 //• Loop through the new_users list to see if each new username has already been used. If it has, print a message that the person will need to enter a new username. 
 //If a username has not been used, print a message saying that the username is available.
-
 //• Make sure your comparison is case insensitive. If 'John' has been used, 'JOHN' should not be accepted.
-
-
 //---------------------------------------------------------------------------------------------------------------------
 //• Make a list of five or more usernames called current_users.
-let current_users : string[] = ["Ayesha", "Bisma", "admin", "Nazish", "usre_4"]
-
+let current_users = ["Ayesha", "Bisma", "admin", "Nazish", "usre_4"];
 //• Make another list of five usernames called new_users. Make sure one or two of the new usernames are also in the current_users list.
-let new_users : string[] = ["ayesha", "bisma", "admin", "usre_5", "usre_6"]
-
-let lowercase = []
-
+let new_users = ["ayesha", "bisma", "admin", "usre_5", "usre_6"];
+let lowercase = [];
 for (let i = 0; i < current_users.length; i++) {
-    lowercase.push(current_users[i].toLowerCase())
-    
+    lowercase.push(current_users[i].toLowerCase());
 }
 //• Loop through the new_users list to see if each new username has already been used. If it has, print a message that the person will need to enter a new username. 
 //If a username has not been used, print a message saying that the username is available.
 for (let i = 0; i < new_users.length; i++) {
-    
     if (lowercase.includes(new_users[i])) {
-    console.log(`${new_users[i]} is not available`);
-        
-    }else{
-        console.log(`${new_users[i]} is available`);
-        
+        console.log(`${new_users[i]} is not available`);
     }
-    
-    
-    
+    else {
+        console.log(`${new_users[i]} is available`);
+    }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+export {};
