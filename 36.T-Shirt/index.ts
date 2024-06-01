76437bdecdf41db6842a6e0f16babad7d0a9f925
ComTech <shumailagulfam3@gmail.com>
2024-06-01 18:37:29 +0500

    first commit

---
+++ 36.T-Shirt/index.ts
@@ -0,0 +1,9 @@
+//Q No 36.
+//T-Shirt: Write a function called make_shirt() that accepts a size and the text of a message that
+// should be printed on the shirt. The function should print a sentence summarizing the size of the shirt and the message printed on it. Call the function.
+
+function make_shirt(size :string, message:string){
+console.log(`making The ${size} size T.Shirt, with Text ${message}`);
+
+}
+make_shirt("mediun" ,"Dream Big, Achieve Bigger" )
